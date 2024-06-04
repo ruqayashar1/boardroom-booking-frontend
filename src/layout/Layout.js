2ec4cfac860bb4093c4dff4eced945af28ef4c95
john <johnmaluki58@gmail.com>
2024-06-04 14:19:12 +0300

    Create login page
    Statically create login page to see how it appears

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "../ components/header/Header";
 import HomePage from "../pages/home_page/HomePage";
+import LoginPage from "../pages/LoginPage";
 
 const Layout = () => {
   return (
     <>
-      <Header />
+      <LoginPage />
+      {/* <Header />
       <main id="main">
         <HomePage />
-      </main>
+      </main> */}
     </>
   );
 };
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,90 @@
 import React from "react";
+import { Oval } from "react-loader-spinner";
+import login_image from "../assets/kemri-logo.png";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <header className="flex justify-center items-center h-[70px] p-2 shadow-sm">
+        <h3 className="font-bold text-[#06ABDD] font-[Inter]">
+          BOARDROOM BOOKING
+        </h3>
+      </header>
+      <section
+        id="login-area"
+        className="flex flex-col justify-center items-center my-8"
+      >
+        <div
+          id="top-loging-area"
+          className="flex flex-col justify-center items-center"
+        >
+          <img src={login_image} alt="logo image" className="w-40 h-40 mb-4" />
+          <h4 className="text-[#DD0606] text-[1.5rem] mb-9 font-bold font-[Poppins] italic drop-shadow-lg">
+            In Search Of Better Health
+          </h4>
+        </div>
+        <form id="login-form" className="font-[Inter]">
+          <div id="login-username" className="">
+            <div className="bg-[#D9D9D9] bg-opacity-[50%] flex justify-around p-1 items-center rounded-sm">
+              <span class="material-symbols-outlined opacity-50 mx-1 mr-2">
+                person
+              </span>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                className="outline-none p-1 bg-[#D9D9D9] bg-opacity-[10%]"
+              />
+              <span class="material-symbols-outlined text-green-500 mx-1">
+                check
+              </span>
+            </div>
+            <span id="error" className="font-thin text-sm text-red-600 mb-2">
+              username required!
+            </span>
+          </div>
+          <div id="login-password" className="mt-2">
+            <div className="bg-[#D9D9D9] bg-opacity-[50%] mb-1 flex justify-around p-1 items-center rounded-sm">
+              <span class="material-symbols-outlined opacity-50 mx-1 mr-2">
+                lock
+              </span>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="outline-none p-1 bg-[#D9D9D9] bg-opacity-[10%]"
+              />
+              <span class="material-symbols-outlined text-green-500 mx-1">
+                check
+              </span>
+            </div>
+            <span id="error" className="font-thin text-sm text-red-600 mb-2">
+              password required!
+            </span>
+          </div>
+          <div
+            id="login-submit"
+            className="relative flex justify-center items-center mt-5"
+          >
+            <input
+              type="submit"
+              value="Login"
+              className="bg-[#06ABDD] text-white font-bold p-2 mr-2 w-24 cursor-pointer rounded-sm"
+            />
+            <Oval
+              visible={true}
+              height="32"
+              width="32"
+              color="#FF956C"
+              ariaLabel="oval-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default LoginPage;
